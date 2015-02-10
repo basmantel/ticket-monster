47b48ea3aeef8018b207b84a55285cb36707ed8d
jimleitch01 <jimleitch01@users.noreply.github.com>
2015-02-10 13:09:24 +0100

    Update tmtest1.py

--- tests/tmtest1.py
+++ tests/tmtest1.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time
+from selenium import webdriver
 
-success = True
-wd = WebDriver()
-wd.implicitly_wait(60)
+sauce_url = "http://jimleitch:3eb84966-ba07-4e0d-98a5-ed1d2f3e2ef8@ondemand.saucelabs.com:80/wd/hub"
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+desired_capabilities = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+
+driver = webdriver.Remote(desired_capabilities=desired_capabilities,
+                          command_executor=sauce_url)
+driver.implicitly_wait(10)
+
 
 try:
     wd.get("http://192.168.4.171:8080/ticket-monster/#about")
